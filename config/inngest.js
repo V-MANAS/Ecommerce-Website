dcc2b3c33f7cb59afc554eeb9f167e50ead00221
Manas Varade <vmanas04040@gmail.com>
2026-02-03 19:50:39 +0530

    New commit

--- config/inngest.js
+++ config/inngest.js
@@ -1,7 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from './db';
 import User from "@/models/User";
-import { Items } from "openai/resources/conversations/items";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
--- next.config.mjs
+++ next.config.mjs
@@ -21,12 +21,15 @@ const nextConfig = {
   experimental: {
     serverActions: {},
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 images: {
-    domains: ['res.cloudinary.com'],
-  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+    },
+  ],
+},
+
 };
 
 export default nextConfig;
